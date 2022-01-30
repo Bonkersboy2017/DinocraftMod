0f2217a0432b8167027e417050ee0efaefc581a6
thomas <amandavanelkan@outlook.com>
2022-01-30 13:42:21 +0000

    started dinotaner

---
+++ src/main/java/com/dinocrew/dinocraft/block/DinotanerBlockEntity.java
@@ -0,0 +1,40 @@
+package com.dinocrew.dinocraft.block;
+
+import com.dinocrew.dinocraft.Dinocraft;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+public class DinotanerBlockEntity extends BlockEntity implements NamedScreenHandlerFactory {
+     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(9, ItemStack.EMPTY);
+
+    public DinotanerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+//    public DinotanerBlockEntity(BlockPos pos, BlockState state) {
+//
+        //    super(Dinocraft,DINOTANER_BLOCK_ENTITY);
+    //}
+
+    @Override
+    public Text getDisplayName() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
+        return null;
+    }
+}
