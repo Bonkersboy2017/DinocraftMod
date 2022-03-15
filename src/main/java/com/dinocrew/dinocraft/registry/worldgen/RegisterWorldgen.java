da1edf4480a236ae7ee8eab3b24b0cadbaca0bd4
thomas <amandavanelkan@outlook.com>
2022-03-15 15:54:06 +0000

    Tree gen

--- src/main/java/com/dinocrew/dinocraft/registry/worldgen/RegisterWorldgen.java
+++ src/main/java/com/dinocrew/dinocraft/registry/worldgen/RegisterWorldgen.java
@@ -4,8 +4,6 @@ import com.dinocrew.dinocraft.Dinocraft;
 import com.dinocrew.dinocraft.registry.ModBlocks;
 import com.dinocrew.dinocraft.registry.ModItems;
 import com.google.common.collect.ImmutableList;
-import com.sun.source.tree.Tree;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.color.world.FoliageColors;
 import net.minecraft.client.sound.MusicType;
@@ -28,63 +26,58 @@ import net.minecraft.world.biome.SpawnSettings;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.blockpredicate.BlockPredicate;
-import net.minecraft.world.gen.decorator.BiomePlacementModifier;
-import net.minecraft.world.gen.decorator.BlockFilterPlacementModifier;
-import net.minecraft.world.gen.decorator.SquarePlacementModifier;
-import net.minecraft.world.gen.decorator.SurfaceWaterDepthFilterPlacementModifier;
+import net.minecraft.world.gen.decorator.*;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.foliage.RandomSpreadFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
 public class RegisterWorldgen {
 
     @Nullable
-
-//    public static PlacedFeature TREES_DRAGONWOOD;
-//    public static ConfiguredFeature<TreeFeatureConfig, ?> DRAGONWOOD;
-
-
     private static final MusicSound DEFAULT_MUSIC = null;
 
     public static final RegistryKey<Biome> BREAKTHROUGH = register("breakthrough");
 
 
-    public static RegistryKey<Biome> register(String name) {
+    public static PlacedFeature TREES_DRAGONWOOD;
+
+
+    public static ConfiguredFeature<TreeFeatureConfig, ?> DRAGONWOOD;
+
+
+
+    private static RegistryKey<Biome> register(String name) {
         return RegistryKey.of(Registry.BIOME_KEY, new Identifier(Dinocraft.MOD_ID, name));
     }
 
 
-
-    public static Biome createBreakthrough() {
+    public static Biome createMangroveSwamp() {
         SpawnSettings.Builder builder = new SpawnSettings.Builder();
         net.minecraft.world.biome.GenerationSettings.Builder builder2 = new net.minecraft.world.biome.GenerationSettings.Builder();
         DefaultBiomeFeatures.addFossils(builder2);
         addBasicFeatures(builder2);
         DefaultBiomeFeatures.addDefaultOres(builder2);
         DefaultBiomeFeatures.addClayDisk(builder2);
-        addBreakthroughFeatures(builder2);
+        addMangroveSwampFeatures(builder2);
         DefaultBiomeFeatures.addDefaultMushrooms(builder2);
         DefaultBiomeFeatures.addSwampVegetation(builder2);
         builder2.feature(GenerationStep.Feature.VEGETAL_DECORATION, OceanPlacedFeatures.SEAGRASS_SWAMP);
         return (
                 new net.minecraft.world.biome.Biome.Builder())
-                .precipitation(Biome.Precipitation.NONE)
-                .category(Biome.Category.SAVANNA)
+                .precipitation(Biome.Precipitation.RAIN)
+                .category(Biome.Category.SWAMP)
                 .temperature(0.8F).downfall(0.9F)
                 .effects((new net.minecraft.world.biome.BiomeEffects.Builder())
-                        .waterColor(0x990099)
-                        .waterFogColor(0x990099)
-                        .fogColor(0x990099)
+                        .waterColor(0x397d71)
+                        .waterFogColor(0x397d71)
+                        .fogColor(12638463)
                         .skyColor(getSkyColor(0.8F))
-                        .foliageColor(0x990099)
+                        .foliageColor(6975545)
                         .grassColorModifier(BiomeEffects.GrassColorModifier.SWAMP)
                         .moodSound(BiomeMoodSound.CAVE).build())
                 .spawnSettings(builder.build())
@@ -92,10 +85,11 @@ public class RegisterWorldgen {
 
     }
 
-    public static void addBreakthroughFeatures(GenerationSettings.Builder builder) {
-//        builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, RegisterWorldgen.TREES_DRAGONWOOD);
-
-
+    public static void addMangroveSwampFeatures(GenerationSettings.Builder builder) {
+        builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, TREES_DRAGONWOOD);
+        builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_WATERLILY);
+        builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.BROWN_MUSHROOM_SWAMP);
+        builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.RED_MUSHROOM_SWAMP);
     }
 
     private static void addBasicFeatures(net.minecraft.world.biome.GenerationSettings.Builder generationSettings) {
@@ -121,27 +115,22 @@ public class RegisterWorldgen {
         return MathHelper.hsvToRgb(0.62222224F - f * 0.05F, 0.5F + f * 0.1F, 1.0F);
     }
 
-
-
-
     public static void RegisterWorldgen() {
-        BuiltinRegistries.add(BuiltinRegistries.BIOME, BREAKTHROUGH, createBreakthrough());
-//
-//        DRAGONWOOD = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Dinocraft.MOD_ID, "dragonwood"), Feature.TREE
-//                .configure(new TreeFeatureConfig.Builder(
-//                        BlockStateProvider.of(ModBlocks.DRAGONWOOD_LOG),
-//                        new StraightTrunkPlacer(8, 3, 0), BlockStateProvider.of(ModBlocks.DRAGONWOOD_LEAVES),
-//                        new RandomSpreadFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), ConstantIntProvider.create(3), 100),
-//                        new TwoLayersFeatureSize(1, 0, 2)).ignoreVines()
-//                        .build()));
-//
-//        TREES_DRAGONWOOD = Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(Dinocraft.MOD_ID, "trees_dragonwood"), DRAGONWOOD.withPlacement(PlacedFeatures.createCountExtraModifier(8, 0.1f, 1), SquarePlacementModifier.of(), SurfaceWaterDepthFilterPlacementModifier.of(6), PlacedFeatures.OCEAN_FLOOR_HEIGHTMAP, BiomePlacementModifier.of(), BlockFilterPlacementModifier.of(BlockPredicate.wouldSurvive(Blocks.ROOTED_DIRT.getDefaultState(), BlockPos.ORIGIN))));
 
+        DRAGONWOOD = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Dinocraft.MOD_ID, "mangrove"), Feature.TREE
+                .configure(new TreeFeatureConfig.Builder(
+                        BlockStateProvider.of(ModBlocks.DRAGONWOOD_LOG),
+                        new StraightTrunkPlacer(8, 3, 0), BlockStateProvider.of(ModBlocks.DRAGONWOOD_LEAVES),
+                        new RandomSpreadFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), ConstantIntProvider.create(3), 100),
+                        new TwoLayersFeatureSize(1, 0, 2)).ignoreVines()
+                        .build()));
+
+
+
+        TREES_DRAGONWOOD = Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(Dinocraft.MOD_ID, "trees_mangrove"), DRAGONWOOD.withPlacement(PlacedFeatures.createCountExtraModifier(8, 0.1f, 1), SquarePlacementModifier.of(), SurfaceWaterDepthFilterPlacementModifier.of(6), PlacedFeatures.OCEAN_FLOOR_HEIGHTMAP, BiomePlacementModifier.of(), BlockFilterPlacementModifier.of(BlockPredicate.wouldSurvive(Blocks.AIR.getDefaultState(), BlockPos.ORIGIN))));
+
+
+        BuiltinRegistries.add(BuiltinRegistries.BIOME, BREAKTHROUGH, createMangroveSwamp());
 
     }
-
-
-
-
-
 }
