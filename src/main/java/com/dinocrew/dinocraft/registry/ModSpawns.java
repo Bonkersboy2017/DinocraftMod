5cb770597cfc8488ff9e8dc01c0435f88976337f
thomas <amandavanelkan@outlook.com>
2022-06-22 17:16:09 +0100

    Fixed mod spawns

--- src/main/java/com/dinocrew/dinocraft/registry/ModSpawns.java
+++ src/main/java/com/dinocrew/dinocraft/registry/ModSpawns.java
@@ -4,9 +4,19 @@ package com.dinocrew.dinocraft.registry;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.tag.BiomeTags;
+import net.minecraft.tag.TagKey;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 
 public class ModSpawns {
     public static void registerAll() {
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), SpawnGroup.MONSTER, ModEntities.SAUROPOD, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), SpawnGroup.MONSTER, ModEntities.TROODON, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), SpawnGroup.MONSTER, ModEntities.THEROPOD, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), SpawnGroup.MONSTER, ModEntities.STEGORAPTOR, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_DEEP_OCEAN), SpawnGroup.MONSTER, ModEntities.MOSASAURUS, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), SpawnGroup.MONSTER, ModEntities.DEINONYCHUS, 1, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_RIVER), SpawnGroup.MONSTER, ModEntities.CHINLEA, 1, 1, 1);
     }
 }
