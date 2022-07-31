d7fd93a25fa5ca000b29ec222fc037aa09b63fec
Treetrain1 <treetrain1@gmail.com>
2022-07-31 05:57:51 -0500

    better liverworts feature

--- src/main/java/net/frozenblock/api/minecraft/worldgen/features/BreakthroughPlants.java
+++ src/main/java/net/frozenblock/api/minecraft/worldgen/features/BreakthroughPlants.java
@@ -1,10 +1,12 @@
 package net.frozenblock.api.minecraft.worldgen.features;
 
 import com.dinocrew.dinocraft.registry.ModBiomes;
+import com.dinocrew.dinocraft.registry.ModBlocks;
 import com.mojang.serialization.Codec;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.frozenblock.registry.RegisterBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.state.property.Properties;
@@ -12,6 +14,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryEntryList;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.GenerationStep;
@@ -25,11 +28,35 @@ import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 public class BreakthroughPlants {
 
     public static final FernFeature BT_FERN = new FernFeature(ProbabilityConfig.CODEC);
-    public static final LiverWortsFeature BT_LIVERWORTS = new LiverWortsFeature(ProbabilityConfig.CODEC);
     public static final CycadophytaFeature BT_CYCADOPHYTA = new CycadophytaFeature(ProbabilityConfig.CODEC);
 
     public static final RegistryEntry<ConfiguredFeature<ProbabilityConfig, ?>> BT_FERN_CONFIGURED = ConfiguredFeatures.register("bt_fern", BT_FERN, new ProbabilityConfig(0.1F));
-    public static final RegistryEntry<ConfiguredFeature<ProbabilityConfig, ?>> BT_LIVERWORTS_CONFIGURED = ConfiguredFeatures.register("bt_liverworts", BT_LIVERWORTS, new ProbabilityConfig(0.1F));
+    public static final RegistryEntry<ConfiguredFeature<MultifaceGrowthFeatureConfig, ?>> BT_LIVERWORTS_CONFIGURED = ConfiguredFeatures.register(
+            "bt_liverworts",
+            Feature.MULTIFACE_GROWTH,
+            new MultifaceGrowthFeatureConfig(
+                    RegisterBlocks.LIVERWORTS,
+                    20,
+                    true,
+                    false,
+                    true,
+                    0.6F,
+                    RegistryEntryList.of(
+                            Block::getRegistryEntry,
+                            ModBlocks.DRAGONGRASS,
+                            ModBlocks.DRAGONWOOD_LOG,
+                            Blocks.DIRT,
+                            Blocks.STONE,
+                            Blocks.ANDESITE,
+                            Blocks.DIORITE,
+                            Blocks.GRANITE,
+                            Blocks.DRIPSTONE_BLOCK,
+                            Blocks.CALCITE,
+                            Blocks.TUFF,
+                            Blocks.DEEPSLATE
+                    )
+            )
+    );
     public static final RegistryEntry<ConfiguredFeature<ProbabilityConfig, ?>> BT_CYCADOPHYTA_CONFIGURED = ConfiguredFeatures.register("bt_cycadophyta", BT_CYCADOPHYTA, new ProbabilityConfig(0.1F));
 
     public static final RegistryEntry<PlacedFeature> BT_FERN_PLACED = PlacedFeatures.register("bt_fern_placed", BT_FERN_CONFIGURED, RarityFilterPlacementModifier.of(1), SquarePlacementModifier.of(), PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
@@ -38,7 +65,6 @@ public class BreakthroughPlants {
 
     public static void registerAll(String modid) {
         Registry.register(Registry.FEATURE, new Identifier(modid, "bt_fern"), BT_FERN);
-        Registry.register(Registry.FEATURE, new Identifier(modid, "bt_liverworts"), BT_LIVERWORTS);
         Registry.register(Registry.FEATURE, new Identifier(modid, "bt_cycadophyta"), BT_CYCADOPHYTA);
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(ModBiomes.BREAKTHHROUGH_KEY),
                 GenerationStep.Feature.TOP_LAYER_MODIFICATION, BT_FERN_PLACED.getKey().get());
