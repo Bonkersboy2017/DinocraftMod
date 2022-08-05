263cc4fe38079bb867c9658507c22fd56c1e8075
thomas <amandavanelkan@outlook.com>
2022-08-05 18:00:09 +0100

    Texture change, removed old textures and commented Microrapor's flying AI out until fix is done

--- src/main/java/com/dinocrew/dinocraft/registry/entities/MicroraptorEntity.java
+++ src/main/java/com/dinocrew/dinocraft/registry/entities/MicroraptorEntity.java
@@ -3,69 +3,78 @@ package com.dinocrew.dinocraft.registry.entities;
 import com.dinocrew.dinocraft.registry.RegisterSounds;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.control.MoveControl;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.FlyingEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-
-public class MicroraptorEntity extends FlyingEntity {
+//THE MICRORAPTOR IS MEANT TO EXTEND FlyingEntity, most of this class is commented out because of a crucial bux that needs fixing.
+public class MicroraptorEntity extends PathAwareEntity  {
     public MicroraptorEntity(EntityType<? extends MicroraptorEntity> entityType, World world) {
         super(entityType, world);
-        this.moveControl = new MicroraptorEntity.MicroraptorMoveControl(this);
+//        this.moveControl = new MicroraptorEntity.MicroraptorMoveControl(this);
     }
 
     protected void initGoals() {
-        this.goalSelector.add(3, new FlyRandomlyGoal(this));
+        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0F));
+        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+//        this.goalSelector.add(3, new FlyRandomlyGoal(this));
     }
+//
+//    public boolean isOnGround() {
+//        return this.onGround;
+//    }
 
-    public boolean isOnGround() {
-        return this.onGround;
-    }
 
-
-    static class MicroraptorMoveControl extends MoveControl {
-        private final MicroraptorEntity microraptor;
-        private int collisionCheckCooldown;
-
-        public MicroraptorMoveControl(MicroraptorEntity microraptor) {
-            super(microraptor);
-            this.microraptor = microraptor;
-        }
-
-        public void tick() {
-            if (this.state == MoveControl.State.MOVE_TO) {
-                if (this.collisionCheckCooldown-- <= 0) {
-                    this.collisionCheckCooldown += this.microraptor.getRandom().nextInt(5) + 2;
-                    Vec3d vec3d = new Vec3d(this.targetX - this.microraptor.getX(), this.targetY - this.microraptor.getY(), this.targetZ - this.microraptor.getZ());
-                    double d = vec3d.length();
-                    vec3d = vec3d.normalize();
-                    if (this.willCollide(vec3d, MathHelper.ceil(d))) {
-                        this.microraptor.setVelocity(this.microraptor.getVelocity().add(vec3d.multiply(0.1D)));
-                    } else {
-                        this.state = MoveControl.State.WAIT;
-                    }
-                }
-
-            }
-        }
-
-        private boolean willCollide(Vec3d direction, int steps) {
-            Box box = this.microraptor.getBoundingBox();
-
-            for (int i = 1; i < steps; ++i) {
-                box = box.offset(direction);
-                if (!this.microraptor.world.isSpaceEmpty(this.microraptor, box)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    }
+//    static class MicroraptorMoveControl extends MoveControl {
+//        private final MicroraptorEntity microraptor;
+//        private int collisionCheckCooldown;
+//
+//        public MicroraptorMoveControl(MicroraptorEntity microraptor) {
+//            super(microraptor);
+//            this.microraptor = microraptor;
+//        }
+//
+//        public void tick() {
+//            if (this.state == MoveControl.State.MOVE_TO) {
+//                if (this.collisionCheckCooldown-- <= 0) {
+//                    this.collisionCheckCooldown += this.microraptor.getRandom().nextInt(5) + 2;
+//                    Vec3d vec3d = new Vec3d(this.targetX - this.microraptor.getX(), this.targetY - this.microraptor.getY(), this.targetZ - this.microraptor.getZ());
+//                    double d = vec3d.length();
+//                    vec3d = vec3d.normalize();
+//                    if (this.willCollide(vec3d, MathHelper.ceil(d))) {
+//                        this.microraptor.setVelocity(this.microraptor.getVelocity().add(vec3d.multiply(0.1D)));
+//                    } else {
+//                        this.state = MoveControl.State.WAIT;
+//                    }
+//                }
+//
+//            }
+//        }
+//
+//        private boolean willCollide(Vec3d direction, int steps) {
+//            Box box = this.microraptor.getBoundingBox();
+//
+//            for (int i = 1; i < steps; ++i) {
+//                box = box.offset(direction);
+//                if (!this.microraptor.world.isSpaceEmpty(this.microraptor, box)) {
+//                    return false;
+//                }
+//            }
+//
+//            return true;
+//        }
+//    }
 
     @Nullable
     @Override
