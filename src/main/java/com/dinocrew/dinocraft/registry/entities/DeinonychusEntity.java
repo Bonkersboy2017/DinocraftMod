a38dfc7ee7c75432016962053f1838695ca82923
Treetrain1 <treetrain1@gmail.com>
2022-08-09 10:29:45 -0500

    not as slow

--- src/main/java/com/dinocrew/dinocraft/registry/entities/DeinonychusEntity.java
+++ src/main/java/com/dinocrew/dinocraft/registry/entities/DeinonychusEntity.java
@@ -25,7 +25,7 @@ public class DeinonychusEntity extends TameableEntity {
     }
 
     protected void initGoals() {
-        this.goalSelector.add(7, new WanderAroundFarGoal(this, 0.3F));
+        this.goalSelector.add(7, new WanderAroundFarGoal(this, 0.7D));
         this.goalSelector.add(2, new MeleeAttackGoal(this, 1.0D, false));
         this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(8, new LookAroundGoal(this));
